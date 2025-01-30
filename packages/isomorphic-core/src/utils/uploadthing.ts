252af5e8d3177440b60af619e4258ce83e71a5ef
Robert Montemayor <rmontemayor0101@gmail.com>
2025-01-29 19:28:43 -0500

    Clean project

--- packages/isomorphic-core/src/utils/uploadthing.ts
+++ packages/isomorphic-core/src/utils/uploadthing.ts
@@ -1,5 +1,5 @@
 import { generateUploadButton, generateUploadDropzone, generateUploader, generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "../../../../apps/isomorphic/src/app/api/uploadthing/core";
+import type { OurFileRouter } from "../../../../apps/ez2erp/src/app/api/uploadthing/core";
 
 export const Uploader: ReturnType<typeof generateUploader> = generateUploader<OurFileRouter>();
 export const UploadButton: ReturnType<typeof generateUploadButton> = generateUploadButton<OurFileRouter>();
